21ce63a9524a8eb85b727715fe39b96e774f68db
Emilio Regalado <quimbolo92@yahoo.com>
2023-06-04 11:59:26 -0400

    updated the SignUp.js and Login.js

--- src/Index.js
+++ src/Index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { createRoot } from 'react-dom';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/navbar.css';
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
